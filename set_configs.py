8f72fd3376a2f09aff4f087f985b021c569d9844
Akanshu Sonkar <akanshu11121@gmail.com>
2025-08-12 22:49:28 +0530

    comfig-changes

--- set_configs.py
+++ set_configs.py
@@ -2,6 +2,6 @@ import os
 import streamlit as st
 
 def set_configuration():
-    os.environ["OPENAI_API_KEY"] = st.secrets["openai"]["api_key"]
-    os.environ["OPENAI_MODEL_NAME"] = st.secrets["openai"]["model_name"]
-    os.environ["SERPER_API_KEY"] = st.secrets["serper"]["api_key"]
+    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+    os.environ["OPENAI_MODEL_NAME"] = st.secrets["OPENAI_MODEL_NAME"]
+    os.environ["SERPER_API_KEY"] = st.secrets["SERPER_API_KEY"]
